0c8d9d63453f58440ec20dc213bbb7c3c144ada5
Klaus-Peter Bernschneider <peter@bernschneider.de>
2025-01-26 10:03:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var SunCalc = require('suncalc');
+var sunrisePos = SunCalc.getPosition(Date.now(), 53.3, 10.1);
+console.log(sunrisePos.azimuth * 180 / Math.PI);
+h = sunrisePos.altitude * 180 / Math.PI;
+document.body.innerHTML = `<p>Sun altitude: ${h} degrees</p>`;
